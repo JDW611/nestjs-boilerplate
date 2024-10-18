f4fda5b01c98538eb4799ba08465f5a8578a2879
다운 <j_d_woon@naver.com>
2024-10-18 17:07:37 +0900

    chore: 🔧 예외처리 필터 및 응답 포맷팅 인터셉터 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/HttpException.filter';
+import { ResponseInterceptor } from './common/interceptors/Response.interceptor';
 
 @Module({
-    imports: [],
+    providers: [
+        {
+            provide: APP_FILTER,
+            useClass: HttpExceptionFilter,
+        },
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ResponseInterceptor,
+        },
+    ],
 })
 export class AppModule {}
